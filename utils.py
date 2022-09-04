c19e91e4b37c3462abe04daac592dc1e35592382
João Antônio Cardoso <joao.maker@gmail.com>
2022-09-04 17:27:47 -0300

    Import utils from the older project.

---
+++ utils.py
@@ -0,0 +1,146 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+from pvlib import irradiance, location
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def integrate(df: pd.DataFrame, time_constant: int = 3600) -> pd.DataFrame:
+    """
+    Integrates a datetime indexed dataframe relative to a time_constant (3600
+    to return in hours)
+    """
+    if df.index.freq is None:
+        df.index.freq = pd.infer_freq(df.index)
+    if df.index.freq is None:
+        raise Exception("Failed infering frequency!")
+    return df.apply(
+        sp.integrate.cumtrapz,
+        initial=0,
+        dx=((df.index.freq.nanos * 1e-9) / time_constant),
+    )
+
+
+def get_irradiance(site_location, tilt, surface_azimuth, weather_data):
+    solar_position = site_location.get_solarposition(times=weather_data.index)
+
+    POA_irradiance = irradiance.get_total_irradiance(
+        surface_tilt=tilt,
+        surface_azimuth=surface_azimuth,
+        dni=weather_data["dni"],
+        ghi=weather_data["ghi"],
+        dhi=weather_data["dhi"],
+        solar_zenith=solar_position["apparent_zenith"],
+        solar_azimuth=solar_position["azimuth"],
+    )
+
+    return pd.DataFrame({"POA": POA_irradiance["poa_global"]})
+
+
+def open_forecast_file(forecast_file: str) -> pd.DataFrame:
+    df = pd.read_csv(forecast_file).rename(
+        columns={
+            "period_end": "PeriodEnd",
+            "air_temp": "AirTemp",
+            "cloud_opacity": "CloudOpacity",
+        }
+    )
+
+    sel = ~df["PeriodEnd"].str.endswith("Z")
+    df.loc[sel, "PeriodEnd"] = df.loc[sel, "PeriodEnd"] + "T00:00:00Z"
+    df.set_index("PeriodEnd", inplace=True)
+    return df
+
+
+def open_forecast_files(forecast_files: list[str], event: dict) -> pd.DataFrame:
+    """Combine all forecasts, always keeping the range interval from the
+    first file from the forecast_files, while using the latest updated
+    forecast data, provided by the consecutive files from the forecast_files."""
+    forecast_files = sorted(set(forecast_files))
+    df = open_forecast_file(forecast_files[0])
+    for i in range(1, len(forecast_files)):
+        df_tmp = open_forecast_file(forecast_files[i])
+        df_tmp = df_tmp.loc[df_tmp.index <= df.index[-1]]
+        df.loc[df.index >= df_tmp.index[0]] = df_tmp
+    return df[event["time"]["start"]: event["time"]["end"]][:-1]
+
+
+def plot_radiation_and_irradiance(
+    ideal_data: pd.DataFrame, real_data: pd.DataFrame, site: location.Location
+):
+    plt.Figure()
+    ideal_data["poa"].plot(label="Clearsky Model")
+    real_data["poa"].plot(label="Solcast Forecast")
+    plt.fill_between(
+        real_data.index,
+        real_data["poa10"],
+        real_data["poa90"],
+        color="orange",
+        alpha=0.3,
+        label="10 to 90 percentile",
+    )
+    plt.ylabel("Irradiance ($W/m^2$)")
+    plt.xlabel("Time ({})".format(site.tz))
+    plt.title(
+        "Hourly Irradiance for {} area (lat={}, lon={}, alt={})".format(
+            site.name, site.latitude, site.longitude, site.altitude
+        )
+    )
+    plt.legend()
+    plt.show()
+
+    plt.Figure()
+    ideal_data["Solar Energy"].plot(label="Clearsky Model")
+    real_data["Solar Energy"].plot(label="Solcast Forecast")
+    plt.fill_between(
+        real_data.index,
+        real_data["Solar Energy10"],
+        real_data["Solar Energy90"],
+        color="orange",
+        alpha=0.3,
+        label="10 to 90 percentile",
+    )
+    plt.ylabel("Energy ($Wh/m^2$)")
+    plt.xlabel("Time ({})".format(site.tz))
+    plt.title(
+        "Hourly Sun Energy for {} area (lat={}, lon={}, alt={})".format(
+            site.name, site.latitude, site.longitude, site.altitude
+        )
+    )
+    plt.legend()
+    plt.show()
+
+
+def plot_energy_bars(
+    ideal_data: pd.DataFrame, real_data: pd.DataFrame, site: location.Location
+):
+    plt.Figure()
+
+    sns.barplot(
+        x=np.datetime_as_string(ideal_data.resample(
+            "D").mean().index.values, unit="D"),
+        y=ideal_data.resample("D").mean()["poa"],
+        label="Clearsky Model",
+        color=sns.color_palette("light:b")[2],
+    )
+    sns.barplot(
+        x=np.datetime_as_string(real_data.resample(
+            "D").mean().index.values, unit="D"),
+        y=real_data.resample("D").mean()["poa"],
+        # yerr=[
+        #     real_data.resample('D').mean()['poa10'],
+        #     real_data.resample('D').mean()['poa90'],
+        # ],
+        label="Solcast Forecast",
+        color=sns.color_palette("light:b")[5],
+    )
+    plt.ylabel("Irradiance ($W/m^2$)")
+    plt.xlabel("Time ({})".format(site.tz))
+    plt.title(
+        "Average Daily Irradiance for {} area (lat={}, lon={}, alt={})".format(
+            site.name, site.latitude, site.longitude, site.altitude
+        )
+    )
+    plt.legend()
+    plt.show()
