a7039def18bd56038878ea2774eefd26de096fa2
João Antônio Cardoso <joao.maker@gmail.com>
2023-02-20 22:55:19 -0300

    boat_model: Completely rewritten BoatModel, fixing all known bugs

--- lib/boat_model.py
+++ lib/boat_model.py
@@ -32,58 +32,42 @@ class Boat:
         # TODO: Create some way to programatically inject an exception, to simulate catastrophic
         # events like crashes, which could take the boat off the race.
 
-        # Step #1 - solve for battery:
-        target_circuits_input_power = self.circuits.power
-        target_pv_output_power = self.panel.solve_output(irradiation)
-        target_esc_input_power = self.propulsion.solve_input(
-            self.motor.solve_input(self.esc.solve_input(motor_throttle))
-        )
-        target_battery_power = (
-            target_pv_output_power
-            - target_esc_input_power
-            - target_circuits_input_power
-        )
-        actual_battery_power = self.battery.solve(dt, target_battery_power)
+        esc_input_target_power = self.esc.solve_input(throttle=motor_throttle)
 
-        # Step #2 - solve for base circuits
-        # if target_circuits_power > actual_battery_power:
-        #     raise Exception("There is no power to keep the basic boat's circuits running!")
-        actual_circuits_input_power = target_circuits_input_power
+        requested_output_target_power = self.circuits.power + esc_input_target_power
 
-        # Step #3 - solve for pv:
-        actual_pv_output_power = (
-            actual_battery_power + target_esc_input_power + actual_circuits_input_power
-        )
-        if actual_pv_output_power > target_pv_output_power:
-            actual_pv_output_power = target_pv_output_power
+        pv_output_target_power = self.panel.solve_output(irradiation=irradiation)
 
-        # Step #4 - solve for motor:
-        actual_esc_input_power = (
-            actual_pv_output_power - actual_battery_power - actual_circuits_input_power
+        battery_charge_target_power = (
+            pv_output_target_power - requested_output_target_power
         )
-        if actual_esc_input_power > target_esc_input_power:
-            actual_esc_input_power = target_esc_input_power
+        battery_charge_power = self.battery.solve(dt, battery_charge_target_power)
 
-        # Step #5 - propagate the power that moves the boat:
-        actual_esc_output_power = self.esc.solve_output(actual_esc_input_power)
-        actual_motor_output_power = self.motor.solve_output(actual_esc_output_power)
-        actual_propulsive_output_power = self.propulsion.solve_output(
-            actual_motor_output_power
+        pv_output_power = min(
+            pv_output_target_power, battery_charge_power + requested_output_target_power
         )
-        actual_hull_speed = self.hull.solve_output(actual_propulsive_output_power)
+
+        esc_input_power = pv_output_power - battery_charge_power - self.circuits.power
+
+        esc_output_power = self.esc.solve_output(esc_input_power)
+        motor_input_power = self.motor.solve_input(esc_output_power)
+        motor_output_power = self.motor.solve_output(motor_input_power)
+        propulsive_input_power = self.propulsion.solve_input(motor_output_power)
+        propulsive_output_power = self.propulsion.solve_output(propulsive_input_power)
+        hull_speed = self.hull.solve_output(propulsive_output_power)
 
         return BoatOutputData(
-            pv_output_power=actual_pv_output_power,
+            pv_output_power=pv_output_power,
             battery_stored_energy=self.battery.energy,
             battery_soc=self.battery.soc,
-            battery_output_power=actual_battery_power,
-            esc_input_power=actual_esc_input_power,
-            esc_output_power=actual_esc_output_power,
-            motor_output_power=actual_esc_output_power,
-            propulsive_output_power=actual_propulsive_output_power,
-            hull_speed=actual_hull_speed,
-            pv_target_power=target_pv_output_power,
-            esc_target_power=target_battery_power,
-            battery_target_power=target_esc_input_power,
+            battery_output_power=battery_charge_power,
+            esc_input_power=esc_input_power,
+            esc_output_power=esc_output_power,
+            motor_output_power=motor_output_power,
+            propulsive_output_power=propulsive_output_power,
+            hull_speed=hull_speed,
+            pv_target_power=pv_output_target_power,
+            esc_target_power=esc_input_target_power,
+            battery_target_power=battery_charge_target_power,
             motor_target_throttle=motor_throttle,
         )
